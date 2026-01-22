051fa4cf586f0a007f2f2021c3ef69a908f38f0f
dxL1nus <linusldx@gmail.com>
2026-01-22 19:56:55 +0100

    Small improvements

--- .codecheck/codecheck.py
+++ .codecheck/codecheck.py
@@ -22,11 +22,11 @@ def name_orcid(entry):
 
 def multiple_name_orcid(entries):
     """Helper function for multiple people to return their Name + ORCID"""
-    return f"{'<br>'.join([name_orcid(a) for a in entries])}"
+    return f"{', '.join([name_orcid(a) for a in entries])}"
 
 def multiple_name(entries):
     """Helper function for multiple people to return their Name + ORCID"""
-    return f"{'<br>'.join([a['name'] for a in entries])}"
+    return f"{', '.join([a['name'] for a in entries])}"
 
 
 class Codecheck:
